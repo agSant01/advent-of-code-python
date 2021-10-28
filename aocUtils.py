6636e1dfcb4037eaa7ef6a86ff3c1fca13365cba
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2021-10-28 17:08:46 -0400

    - Added download of instructions as markdown - `Requiremente.txt`

--- aocUtils.py
+++ aocUtils.py
@@ -1,13 +1,15 @@
-import requests
-import datetime
 import argparse
-from datetime import datetime
+import datetime
 import os
+import re
+from datetime import datetime
 
-from templates \
-    import \
-    day as dayutils, \
-    year as yearutils
+import requests
+from bs4 import BeautifulSoup
+from markdownify import markdownify
+
+from templates import day as dayutils
+from templates import year as yearutils
 
 
 def console(msg):
@@ -49,6 +51,47 @@ def init_year(year: int = None):
     with open(fp, 'w') as f:
         for l in yearutils.lines():
             f.write(l + '\n')
+
+
+def create_instruction_file(day, year=None):
+    if not year:
+        console(
+            '[ERROR] To download instruction files the year must be specified...')
+        exit(1)
+
+    file_path = os.path.join(str(year), f'day{day}.md')
+    console(f'Downloading instructions file: {file_path}')
+
+    complete_url = f'{BASE_URL}/{year}/day/{int(day)}'
+
+    console(f'GET at {complete_url}')
+
+    headers = dict(cookie=CONFIG['SESSION_COOKIE'])
+    data = requests.get(complete_url, headers=headers)
+
+    if data.status_code == 200:
+        console(f'Success:  {data.status_code}')
+    else:
+        console(f'Error:  {data.status_code}')
+        console(f'Response Content: {data.content}')
+        exit(1)
+
+    soup = BeautifulSoup(data.content.decode(), 'html.parser')
+
+    tags = soup.find_all('article', class_=re.compile('day-desc'))
+
+    def remove_dash(tag):
+        str_ = tag.h2.string.replace('---', '').strip()
+        tag.h2.string.replace_with(str_)
+        return str(tag)
+
+    markdown = markdownify(
+        ''.join(map(remove_dash, tags)), heading_style='ATX')
+
+    with open(file_path, 'w') as f:
+        f.write(markdown)
+
+    console('Created instrutions MD file...')
 
 
 def create_input_files(day, data, year=None, force=False):
@@ -101,7 +144,6 @@ def get_input_data(day, year=None, force=False):
 
 
 def create_day(day: str, year: str = None):
-
     day = str(day).zfill(2)
     if not year:
         year = datetime.today().year
@@ -119,6 +161,7 @@ def create_day(day: str, year: str = None):
             f.write(l + '\n')
 
     get_input_data(day, year)
+    create_instruction_file(day, year)
 
 
 def run(day, year=None):
@@ -139,6 +182,9 @@ def main():
                         type=int, required=False)
     parser.add_argument('--input-data', '-i',
                         type=int, required=False)
+    parser.add_argument('--day-desc', '-dd', type=int,
+                        default=None, required=False, help='Day description. Download the instructions of the day at AOC as Markdown.'
+                        'Will only be able to download unlocked instructions. Would not download "Part 2" if "Part 1" is not completed.')
 
     parser.add_argument('--run', '-r',
                         type=int, required=False)
@@ -166,6 +212,8 @@ def main():
         create_day(args.create_day, args.year)
     elif args.input_data:
         get_input_data(args.input_data, args.year, args.force)
+    elif args.day_desc:
+        create_instruction_file(args.day_desc, args.year)
 
 
 if __name__ == "__main__":
