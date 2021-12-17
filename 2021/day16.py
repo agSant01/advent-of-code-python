650ae2bff710363c2084f18de8e05aeb326f8e1b
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2021-12-16 23:44:00 -0400

    Update challenge 2021-16 to use bit array instead of bit string. Memory mngmt improvment.

--- 2021/day16.py
+++ 2021/day16.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Union
+from typing import List, Union
 
 
 def get_filename(test=False):
@@ -31,17 +31,9 @@ class Packet:
         self.version = version
         self.typeId = typeId
         self.lenTypeId = None
-        self._value = None
+        self.value = None
         self.subPackets = []
         self.bitLen = None
-
-    @property
-    def value(self) -> str:
-        return self._value
-
-    @value.setter
-    def value(self, value: List[str]):
-        self._value = ''.join(value)
 
     def __str__(self) -> str:
         if self.value:
@@ -51,15 +43,54 @@ class Packet:
         return f'<Packet version:{self.version} typeId:{self.typeId} lenTypeId:{self.lenTypeId} contents:{content} bitLen:{self.bitLen}>'
 
 
-def hex_to_bin(hexstr: str):
-    return bin(int(hexstr, 16))[2:].rjust(len(hexstr)*4, '0')
+HEX_TO_BIT = {
+    '0': [0, 0, 0, 0],
+    '1': [0, 0, 0, 1],
+    '2': [0, 0, 1, 0],
+    '3': [0, 0, 1, 1],
+    '4': [0, 1, 0, 0],
+    '5': [0, 1, 0, 1],
+    '6': [0, 1, 1, 0],
+    '7': [0, 1, 1, 1],
+    '8': [1, 0, 0, 0],
+    '9': [1, 0, 0, 1],
+    'A': [1, 0, 1, 0],
+    'B': [1, 0, 1, 1],
+    'C': [1, 1, 0, 0],
+    'D': [1, 1, 0, 1],
+    'E': [1, 1, 1, 0],
+    'F': [1, 1, 1, 1],
+}
 
 
-def parse_packet(packet: str) -> Packet:
+def hex_to_bit_arr(hexstr: str):
+    bitarr = []
+    for hex_ in hexstr:
+        bitarr.extend(HEX_TO_BIT[hex_])
+    return bitarr
+
+
+def bit_arr_to_dec(bitArr: List[bool]) -> int:
+    """Convert bit array to decimal representation
+
+    Args:
+        bitArr (List[bool]): bit array
+
+    Returns:
+        int: int representation of bit array
+    """
+    value: int = 0
+    for bit in bitArr:
+        value = value << 1
+        value += bit
+    return value
+
+
+def parse_packet(packet: List[bool]) -> Packet:
     # Uncomment to see process
 
-    version = int(packet[0:3], 2)
-    typeId = int(packet[3:6], 2)
+    version = bit_arr_to_dec(packet[0:3])
+    typeId = bit_arr_to_dec(packet[3:6])
 
     packetObj: Packet = Packet(version, typeId)
 
@@ -69,10 +100,10 @@ def parse_packet(packet: str) -> Packet:
         # Data packet
         values = []
         curr = 6
-        while packet[curr] != '0':
-            values.append(packet[curr+1:curr+5])
+        while packet[curr] != 0:
+            values.extend(packet[curr+1:curr+5])
             curr += 5
-        values.append(packet[curr+1:curr+5])
+        values.extend(packet[curr+1:curr+5])
         packetObj.value = values
         packetObj.bitLen = curr+5
         return packetObj
@@ -85,9 +116,10 @@ def parse_packet(packet: str) -> Packet:
 
     # print('I:', len_type_id)
 
-    if packetObj.lenTypeId == '0':
+    if packetObj.lenTypeId == 0:
         bitsToLook = 15
-        lenInBits = int(packet[HEADER_OFFSET:HEADER_OFFSET+bitsToLook], 2)
+        lenInBits = bit_arr_to_dec(
+            packet[HEADER_OFFSET:HEADER_OFFSET+bitsToLook])
         curr = HEADER_OFFSET+bitsToLook
         while curr < lenInBits+(HEADER_OFFSET+bitsToLook):
             subPacket: Packet = parse_packet(packet[curr:curr+lenInBits])
@@ -95,8 +127,8 @@ def parse_packet(packet: str) -> Packet:
             curr += subPacket.bitLen
     else:
         bitsToLook = 11
-        totalSubPackets = int(
-            packet[HEADER_OFFSET:HEADER_OFFSET+bitsToLook], 2)
+        totalSubPackets = bit_arr_to_dec(
+            packet[HEADER_OFFSET:HEADER_OFFSET+bitsToLook])
         curr = HEADER_OFFSET+bitsToLook
         while totalSubPackets > 0:
             subPacket: Packet = parse_packet(packet[curr:])
@@ -130,8 +162,8 @@ def day16p1():
 
     result = []
     for packet in packets:
-        # print(packet, 'Len Original:', len(packet)*4)
-        packet = hex_to_bin(packet)
+        # hex_to_bit_arr(packet, 'Len Original:', len(packet)
+        packet = hex_to_bit_arr(packet)
         syntax_tree = parse_packet(packet)
         print('ST:', syntax_tree)
         packetVersionSum = sum_packet_versions(syntax_tree)
@@ -156,7 +188,7 @@ def execute_packets(packet: Packet) -> Union[int, bool]:
     typeId = packet.typeId
 
     if typeId == 4:
-        return int(packet.value, 2)
+        return bit_arr_to_dec(packet.value)
 
     if typeId == 0:
         # sum
@@ -212,7 +244,7 @@ def day16p2():
     packets = get_input(parse2, test=False)
     results = []
     for packet in packets:
-        packet = hex_to_bin(packet)
+        packet = hex_to_bit_arr(packet)
         st = parse_packet(packet)
         result = execute_packets(st)
         results.append(result)
